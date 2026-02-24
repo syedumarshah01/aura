8dc87959140689d0b8e70e7cef8227216319187b
Syed Umar Shah <syedumarshahgithub@gmail.com>
2026-02-24 22:19:19 +0500

    change port to 8080 on backend

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
